b3b014ea04a5047dac7d8915cccecc2e0c47ac09
achmadkurniansyah <65005697+achmadkurniansyah@users.noreply.github.com>
2020-12-03 21:12:27 +0700

    Menghapus semua code

--- latihan_intro_1.py
+++ latihan_intro_1.py
@@ -1,13 +1,1 @@
-# # Cara 1, inputan tetap string tapi di dalam printan dijadikan integer baru jadi string lagi
-# nama = input('Masukkan Nama: ')
-# umur = input('Masukkan Umur: ')
-# print('Umur saya 5 tahun kedepan adalah: ' + str(int(umur)+5))
-# print('Umur saya jika di modulus 2 adalah: ' +str(int(umur)%2))
-
-# # Cara 2, inputan dijadikan Integer terlebih dahulu
-# nama = input('Masukkan Nama: ')
-# umur = int(input('Masukkan Umur: '))
-# print('Umur saya 5 tahun kedepan adalah: ' +str(umur+5))
-# print('Umur saya jika di modulus 2 adalah: ' +str(umur%2))
-# usiaAndi = '40'
-# usiaBudi = 20
+#FILE INI KOSONG
